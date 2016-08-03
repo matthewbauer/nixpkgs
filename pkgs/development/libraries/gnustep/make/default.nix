70acf23b951d224dcc5103736f15ce59afd78ba9
Matthew Bauer <mjbauer95@gmail.com>
2016-08-02 19:38:06 -0500

    gnustep: set with-layout=fhs
    This will preent OS X from having compat issues.

--- pkgs/development/libraries/gnustep/make/default.nix
+++ pkgs/development/libraries/gnustep/make/default.nix
@@ -8,7 +8,10 @@ stdenv.mkDerivation rec {
     url = "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-make-${version}.tar.gz";
     sha256 = "0r00439f7vrggdwv60n8p626gnyymhq968i5x9ad2i4v6g8x4gk0";
   };
-  configureFlags = "--with-installation-domain=SYSTEM";
+  configureFlags = [
+    "--with-installation-domain=SYSTEM"
+    "--with-layout=fhs"
+  ];
   buildInputs = [ clang which libobjc ];
   patches = [ ./fixup-paths.patch ];
   setupHook = ./setup-hook.sh;
