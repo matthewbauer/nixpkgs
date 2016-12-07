c3307fd02bb5bacf517df348c8797c78d0882ccc
Matthew Bauer <mjbauer95@gmail.com>
2016-12-06 19:28:49 -0600

    virtualbox: fixup, add qt-plus

--- pkgs/applications/virtualization/virtualbox/darwin.nix
+++ pkgs/applications/virtualization/virtualbox/darwin.nix
@@ -1,7 +1,8 @@
 { stdenv, fetchurl, lib, xcbuild, libIDL, pkgconfig, which
 , openssl, curl, SDL, glib, libxslt, python, perl
 , cf-private, DirectoryService
-, qt5 }:
+, qt5
+, runCommand }:
 
 with stdenv.lib;
 
@@ -10,7 +11,18 @@ let
 
   inherit (importJSON ./upstream-info.json) version main;
 
-in stdenv.mkDerivation {
+  # we need to modify qt dir to get build working
+  qt-plus = runCommand "qt-plus" {} ''
+    mkdir -p $out/Frameworks
+    ln -s \
+      ${qt5.qtbase}/lib/* \
+      ${qt5.qtmacextras}/lib/QtMacExtras.framework \
+      $out/Frameworks
+    ln -s ${qt5.qtbase}/lib/qt5/* $out/Frameworks
+    ln -s ${qt5.qtbase}/* $out
+  '';
+
+in stdenv.mkDerivation rec {
   name = "virtualbox-${version}";
 
   src = fetchurl {
@@ -26,17 +38,22 @@ in stdenv.mkDerivation {
     export USER=nix
 
     sed -i \
-      -e 's|CC="gcc"|CC="clang"|' \
-      -e 's|CXX="g++"|CXX="clang++"|' \
+      -e 's|CC="gcc"|CC="cc"|' \
+      -e 's|CXX="g++"|CXX="c++"|' \
       -e 's|^check_gcc$|# check_gcc|' \
       -e 's|^LIBCRYPTO=".*"$|LIBCRYPTO="-L${openssl.out}/lib -lcrypto -lssl"|' \
       -e 's|^INCCRYPTO=""|INCCRYPTO="-I${openssl.dev}/include"|' \
       -e 's@MKISOFS --version@MKISOFS -version@' \
       -e 's@PYTHONDIR=.*@PYTHONDIR=${python}@' \
+      -e 's@TOOLQT5BIN=.*@TOOLQT5BIN="${getDev qt5.qtbase}/bin"@' \
       configure
 
     substituteInPlace Config.kmk \
        --replace "/usr/bin/xcodebuild" "/bin/xcodebuild"
+
+    sed -i \
+       -e 's|typedef _Bool bool;|#undef bool\ntypedef _Bool bool;|g' \
+       include/iprt/types.h
   '';
 
   configurePhase = ''
@@ -60,15 +77,21 @@ in stdenv.mkDerivation {
     VBOX_PATH_MACOSX_SDK_ROOT      := ${xcbuild.platform}/Developer//SDKs
     VBOX_PATH_MACOSX_TOOLCHAIN_ROOT:= ${xcbuild.toolchain}
 
-    PATH_QT5_X11_EXTRAS_LIB        := ${getLib qt5.qtx11extras}/lib
-    PATH_QT5_X11_EXTRAS_INC        := ${getDev qt5.qtx11extras}/include
-    TOOL_QT5_LRC                   := ${getDev qt5.qttools}/bin/lrelease
+    TOOL_GXX4MACHO_PREFIX          :=
+    TOOL_GXX4MACHO_SUFFIX          :=
+    TOOL_GCC4MACHO_PREFIX          :=
+    TOOL_GCC4MACHO_SUFFIX          :=
+
+    TOOL_GXX32_CC := cc
+    TOOL_GXX32_CXX := c++
 
     LOCAL_CONFIG
 
     ./configure \
       --disable-hardening \
-      --with-xcode-dir=${xcbuild}
+      --with-xcode-dir=${xcbuild} \
+      --enable-qt5 \
+      --with-qt-dir=${qt-plus}
 
     sed -e 's@PKG_CONFIG_PATH=.*@PKG_CONFIG_PATH=${libIDL}/lib/pkgconfig:${glib.dev}/lib/pkgconfig ${libIDL}/bin/libIDL-config-2@' \
         -i AutoConfig.kmk
@@ -77,9 +100,11 @@ in stdenv.mkDerivation {
   nativeBuildInputs = [ pkgconfig which xcbuild ];
   buildInputs = [ libIDL openssl curl SDL glib libxslt
                   python cf-private DirectoryService perl
-                  qt5.qtbase qt5.qtx11extras ];
+                  qt5.qtbase qt5.qtmacextras qt5.qttools.dev ];
 
   dontUseXcbuild = true;
+
+  NIX_CFLAGS_COMPILE = " -DRT_OS_DARWIN ";
 
   buildPhase = ''
     export NIX_CFLAGS_COMPILE=" -F${cf-private}/Library/Frameworks $NIX_CFLAGS_COMPILE"
