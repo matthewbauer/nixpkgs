ad41b1cf0b9862d765f016a51240009c13f83a6e
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-05-20 19:44:20 +0300

    nixpkgs release: Fix Darwin-only jobs
    Currently the logic of generating nixpkgs Hydra jobs is to walk through
    the pkgs evaluated for system = "x86_64-linux", collect any derivations
    and their meta.platforms values. However, that doesn't work for
    packages whose meta.platforms doesn't include x86_64-linux, as just
    evaluating their meta attribute raises an error so they get skipped
    completely.
    
    As a less-intrusive fix (i.e. anything than rewriting the current package
    enumeration logic), allow passing `config.allowUnsupportedSystem = true`
    to permit evaluating packages regardless of their platform and use that
    in the package listing phase.

--- pkgs/stdenv/generic/check-meta.nix
+++ pkgs/stdenv/generic/check-meta.nix
@@ -40,6 +40,8 @@ let
 
   allowBroken = config.allowBroken or false || builtins.getEnv "NIXPKGS_ALLOW_BROKEN" == "1";
 
+  allowUnsupportedSystem = allowBroken || config.allowUnsupportedSystem or false;
+
   isUnfree = licenses: lib.lists.any (l:
     !l.free or true || l == "unfree" || l == "unfree-redistributable") licenses;
 
@@ -175,7 +177,7 @@ let
       { valid = false; reason = "unfree"; errormsg = "has an unfree license (‘${showLicense attrs.meta.license}’)"; }
     else if hasBlacklistedLicense attrs then
       { valid = false; reason = "blacklisted"; errormsg = "has a blacklisted license (‘${showLicense attrs.meta.license}’)"; }
-    else if !allowBroken && attrs.meta.broken or false then
+    else if !allowUnsupportedSystem && attrs.meta.broken or false then
       { valid = false; reason = "broken"; errormsg = "is marked as broken"; }
     else if !allowBroken && attrs.meta.platforms or null != null && !lib.lists.elem system attrs.meta.platforms then
       { valid = false; reason = "broken"; errormsg = "is not supported on ‘${system}’"; }
