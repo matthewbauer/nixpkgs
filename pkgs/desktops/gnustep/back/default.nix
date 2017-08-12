f9aabc7445d656d6780967f2754a2f080da44332
Matthew Bauer <mjbauer95@gmail.com>
2017-08-12 02:26:27 -0700

    gnustep.back: 0.25.0 -> 0.25.1

--- pkgs/desktops/gnustep/back/default.nix
+++ pkgs/desktops/gnustep/back/default.nix
@@ -8,13 +8,13 @@
 , libXmu
 }:
 let
-  version = "0.25.0";
+  version = "0.25.1";
 in
 gsmakeDerivation {
   name = "gnustep-back-${version}";
   src = fetchurl {
     url = "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-back-${version}.tar.gz";
-    sha256 = "14gs1b32ahnihd7mwpjrws2b8hl11rl1wl24a7651d3z2l7f6xj2";
+    sha256 = "14gs1b32ahnihd7mwpjrws2b8hl11rl1wl24a7651d3z2l7f6xj0";
   };
   buildInputs = [ cairo base gui freetype pkgconfig x11 libXmu ];
   meta = {
