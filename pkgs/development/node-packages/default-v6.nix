e34a17f4e4d5ef95ecbc0c52a9d509433c9098f2
Matthew Bauer <mjbauer95@gmail.com>
2018-03-08 17:34:59 -0600

    nodePackages: rerun generate.sh

--- pkgs/development/node-packages/default-v6.nix
+++ pkgs/development/node-packages/default-v6.nix
@@ -24,13 +24,13 @@ nodePackages // {
   phantomjs = nodePackages.phantomjs.override (oldAttrs: {
     buildInputs = oldAttrs.buildInputs ++ [ pkgs.phantomjs2 ];
   });
-  
+
   webdrvr = nodePackages.webdrvr.override (oldAttrs: {
     buildInputs = oldAttrs.buildInputs ++ [ pkgs.phantomjs ];
-    
+
     preRebuild = ''
       mkdir $TMPDIR/webdrvr
-      
+
       ln -s ${pkgs.fetchurl {
         url = "https://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar";
         sha1 = "ef1b5f8ae9c99332f99ba8794988a1d5b974d27b";
