5e2a66c55cc491d01218e57a8c3445275c4d71e1
Matthew Bauer <mjbauer95@gmail.com>
2018-06-05 23:18:04 -0400

    Fixup libiconv
    - remove private symbols
    - build statically

--- pkgs/os-specific/darwin/apple-source-releases/libiconv/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/libiconv/default.nix
@@ -3,16 +3,7 @@
 appleDerivation {
   preConfigure = "cd libiconv";
 
-  postInstall = ''
-    mv $out/lib/libiconv.dylib $out/lib/libiconv-nocharset.dylib
-    ${stdenv.cc.binutils.prefix}install_name_tool -id $out/lib/libiconv-nocharset.dylib $out/lib/libiconv-nocharset.dylib
-
-    # re-export one useless symbol; ld will reject a dylib that only reexports other dylibs
-    echo 'void dont_use_this(){}' | ${stdenv.cc.binutils.prefix}clang -dynamiclib -x c - -current_version 2.4.0 \
-      -compatibility_version 7.0.0 -current_version 7.0.0 -o $out/lib/libiconv.dylib \
-      -Wl,-reexport_library -Wl,$out/lib/libiconv-nocharset.dylib \
-      -Wl,-reexport_library -Wl,$out/lib/libcharset.dylib
-  '';
+  configureFlags = [ "--enable-static" ];
 
   meta = {
     platforms = stdenv.lib.platforms.darwin;
