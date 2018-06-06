2075dde9d8bfe15c1ae2af6bfce1496990c5ad8a
Matthew Bauer <mjbauer95@gmail.com>
2018-06-05 23:45:27 -0400

    libiconv: also remove charset stuff
    GHC will pull it in if it’s available in the headers.
    
    See:
    
    https://github.com/ghc/packages-base/blob/master/configure.ac#L194-L204

--- pkgs/os-specific/darwin/apple-source-releases/libiconv/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/libiconv/default.nix
@@ -3,6 +3,10 @@
 appleDerivation {
   preConfigure = "cd libiconv";
 
+  preFixup = ''
+    rm -f $out/include/libcharset.h $out/include/localcharset.h
+  '';
+
   configureFlags = [ "--enable-static" ];
 
   meta = {
