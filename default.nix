5c80db9286b0488f048cf3734824056f08aa912c
Matthew Bauer <matthew.bauer@obsidian.systems>
2018-07-16 14:10:43 -0400

    default.nix: nix long lines

--- default.nix
+++ default.nix
@@ -1,18 +1,22 @@
 let requiredVersion = import ./lib/minver.nix; in
 
-if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
+if ! builtins ? nixVersion ||
+   builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
 
   abort ''
 
     This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade:
 
-    - If you are running NixOS, `nixos-rebuild' can be used to upgrade your system.
+    - If you are running NixOS, `nixos-rebuild' can be used to upgrade
+      your system.
 
-    - Alternatively, with Nix > 2.0 `nix upgrade-nix' can be used to imperatively
-      upgrade Nix. You may use `nix-env --version' to check which version you have.
+    - Alternatively, with Nix > 2.0 `nix upgrade-nix' can be used to
+      imperatively upgrade Nix. You may use `nix-env --version' to
+      check which version you have.
 
-    - If you installed Nix using the install script (https://nixos.org/nix/install),
-      it is safe to upgrade by running it again:
+    - If you installed Nix using the install script
+      (https://nixos.org/nix/install), it is safe to upgrade by
+      running it again:
 
           curl https://nixos.org/nix/install | sh
   ''
