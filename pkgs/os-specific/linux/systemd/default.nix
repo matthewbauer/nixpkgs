003f37b9ae02bc6c11397a1eb803be5c99ab1618
Matthew Bauer <mjbauer95@gmail.com>
2020-03-11 16:53:04 -0400

    systemd: don’t add host suffix
    Linux has a limit of 127 characters in kernel.core_pattern. On native
    builds this looks like:
    
    /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-systemd-243.4/lib/systemd/systemd-coredump %P %u %g %s %t %c %e
    
    (107 characters)
    
    In cross, this path gets up to 30 characters
    more (armv6l-unknown-linux-gnueabihf), which means Linux cuts off the
    last few parameters. To fix this, avoid adding host suffix to systemd.

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -34,6 +34,10 @@ in stdenv.mkDerivation {
   version = "245.3";
   pname = "systemd";
 
+  # extra suffix adds more characters than linux can handle for
+  # kernel.core_pattern (127)
+  dontAddHostSuffix = true;
+
   # When updating, use https://github.com/systemd/systemd-stable tree, not the development one!
   # Also fresh patches should be cherry-picked from that tree to our current one.
   src = fetchFromGitHub {
