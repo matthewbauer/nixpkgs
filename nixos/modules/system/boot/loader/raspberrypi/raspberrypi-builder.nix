250b61450b9798f0731ada40582661b618b6b768
Matthew Bauer <mjbauer95@gmail.com>
2020-03-13 00:16:45 -0400

    raspberrypi: make raspberrypifw custom
    This allows us to cross compile the loader by overriding raspberrypifw
    and pkgs like:
    
      raspberrypi-conf-builder =
        import <nixpkgs/nixos/modules/system/boot/loader/raspberrypi/raspberrypi-builder.nix> {
          pkgs = pkgs.buildPackages;
          raspberrypifw = pkgs.raspberrypifw;
          configTxt = "...";
        };

--- nixos/modules/system/boot/loader/raspberrypi/raspberrypi-builder.nix
+++ nixos/modules/system/boot/loader/raspberrypi/raspberrypi-builder.nix
@@ -1,10 +1,10 @@
-{ pkgs, configTxt }:
+{ pkgs, configTxt, raspberrypifw ? pkgs.raspberrypifw }:
 
 pkgs.substituteAll {
   src = ./raspberrypi-builder.sh;
   isExecutable = true;
   inherit (pkgs.buildPackages) bash;
   path = [pkgs.buildPackages.coreutils pkgs.buildPackages.gnused pkgs.buildPackages.gnugrep];
-  firmware = pkgs.raspberrypifw;
+  firmware = raspberrypifw;
   inherit configTxt;
 }
