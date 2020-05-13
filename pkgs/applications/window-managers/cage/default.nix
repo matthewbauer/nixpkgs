c209a94627be097d6bf8d461c05e18d86165a85b
Matthew Bauer <mjbauer95@gmail.com>
2020-05-12 22:55:35 -0500

    cage: wrap with correct bash

--- pkgs/applications/window-managers/cage/default.nix
+++ pkgs/applications/window-managers/cage/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchFromGitHub
 , meson, ninja, pkg-config, wayland, scdoc, makeWrapper
 , wlroots, wayland-protocols, pixman, libxkbcommon
-, systemd, libGL, libX11, mesa
+, systemd, libGL, libX11, mesa, runtimeShell
 , xwayland ? null
 , nixosTests
 }:
@@ -31,6 +31,7 @@ stdenv.mkDerivation rec {
 
   postFixup = lib.optionalString (xwayland != null) ''
     wrapProgram $out/bin/cage --prefix PATH : "${xwayland}/bin"
+    sed -i 's,${stdenv.shell},${runtimeShell},' $out/bin/cage
   '';
 
   # Tests Cage using the NixOS module by launching xterm:
