c94dff92f1abd8e415deefeb771b39d2a7688c45
Matthew Bauer <mjbauer95@gmail.com>
2020-08-23 23:53:31 -0500

    cog: init at 0.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11501,6 +11501,8 @@ in
 
   cointop = callPackage ../applications/misc/cointop { };
 
+  cog = callPackage ../development/web/cog { };
+
   ctl = callPackage ../development/libraries/ctl { };
 
   ctpp2 = callPackage ../development/libraries/ctpp2 { };
