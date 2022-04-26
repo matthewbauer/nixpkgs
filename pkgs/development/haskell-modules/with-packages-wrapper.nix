d142881f4318260f56f9ff8baedd9197c712024f
Matthew Bauer <mjbauer95@gmail.com>
2022-04-26 11:17:19 -0500

    Revert "Fix withLLVM check in haskell-modules/with-packages-wrapper.nix"
    This reverts commit 51fdaf4188302f1ffafbe59e573e8a1456197e91.

--- pkgs/development/haskell-modules/with-packages-wrapper.nix
+++ pkgs/development/haskell-modules/with-packages-wrapper.nix
@@ -1,5 +1,5 @@
 { lib, stdenv, ghc, llvmPackages, packages, symlinkJoin, makeWrapper
-, withLLVM ? !(stdenv.hostPlatform.isx86_64 || stdenv.hostPlatform.isPowerPC || (stdenv.hostPlatform.isAarch64 && stdenv.hostPlatform.isDarwin))
+, withLLVM ? !(stdenv.hostPlatform.isx86_64 || stdenv.hostPlatform.isPowerPC)
 , postBuild ? ""
 , ghcLibdir ? null # only used by ghcjs, when resolving plugins
 }:
